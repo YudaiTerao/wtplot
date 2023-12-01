e3822a0482bb346d1665cc8d07bb7a9c1173420d
yudai <terao.yudai.s4@dc.tohoku.ac.jp>
2023-12-02 01:18:49 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="qEplot",
-    version="0.2.0",
+    name="wtplot",
+    version="0.1.0",
     author="Yudai Terao",
     packages=find_packages(),
     install_requires=[
         'plottool@git+https://github.com/YudaiTerao/plottool.git'
+        'BZplot@git+https://github.com/YudaiTerao/BZplot.git'
     ],
     entry_points={
         'console_scripts':[
-            'qb = qEplot.banddos_plot:bandplot',
-            'qbp = qEplot.banddos_plot:banddosplot',
-            'qbc = qEplot.banddos_plot:qbwbplot',
-            'qb-pdf = qEplot.banddos_plot_pdf:bandplot',
-            'qbp-pdf = qEplot.banddos_plot_pdf:banddosplot',
-            'qbc-pdf = qEplot.banddos_plot_pdf:qbwbplot',
+            'ahc     = wtplot.ahcplot:main',
+            'anc     = wtplot.ancplot:main',
+            'anccalc = wtplot.anccalc:main',
+            'curv    = wtplot.curvplot:main',
+            'bdgap   = wtplot.bandgapplot:main',
+            'bdplane = wtplot.band_plane_plot:main',
         ],
     },
 )
